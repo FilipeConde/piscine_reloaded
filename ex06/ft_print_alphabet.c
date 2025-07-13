0bec4ea34a9dc65252b3c35cf40d96c18f3db36a
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-13 14:27:25 -0300

    Ajusta entrega removendo função main() e definição de ft_putchar().

--- ex06/ft_print_alphabet.c
+++ ex06/ft_print_alphabet.c
@@ -6,15 +6,11 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 18:06:06 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/13 14:13:06 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/13 14:23:56 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
 
 void	ft_print_alphabet(void)
 {
@@ -26,9 +22,4 @@ void	ft_print_alphabet(void)
 		ft_putchar(curr_char);
 		curr_char++;
 	}
-}
-
-int	main(void)
-{
-	ft_print_alphabet();
 }
