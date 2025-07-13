08176e7c3f2dd1d02105fd48310d17be50118bbc
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-13 18:03:26 -0300

    Ajusta arquivo de acordo com a norma para entrega.

--- ex11/ft_div_mod.c
+++ ex11/ft_div_mod.c
@@ -6,31 +6,12 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 17:24:51 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/13 18:01:35 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/13 18:02:50 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 void	ft_div_mod(int a, int b, int *div, int *mod)
 {
 	*div = a / b;
 	*mod = a % b;
-}
-
-int	main(void)
-{
-	int	a;
-	int	b;
-	int	div;
-	int	mod;
-	int	*ptr_div;
-	int	*ptr_mod;
-
-	a = 5;
-	b = 3;
-	ptr_div = &div;
-	ptr_mod = &mod;
-	ft_div_mod(a, b, ptr_div, ptr_mod);
-	printf("%d - %d\n", div, mod);
 }
