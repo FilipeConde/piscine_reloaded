831e2e0bc4e19163cac275a8ffc0c6598d0971a2
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-13 15:52:40 -0300

    Prepara entrega de ex08 segundo normas.

--- ex08/ft_is_negative.c
+++ ex08/ft_is_negative.c
@@ -6,16 +6,11 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 15:44:16 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/13 15:45:46 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/13 15:47:25 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
 
 void	ft_is_negative(int n)
 {
@@ -27,12 +22,4 @@ void	ft_is_negative(int n)
 	{
 		ft_putchar('P');
 	}
-}
-
-int	main(void)
-{
-	ft_is_negative(-1);
-	ft_is_negative(0);
-	ft_is_negative(1);
-	ft_is_negative(10);
 }
