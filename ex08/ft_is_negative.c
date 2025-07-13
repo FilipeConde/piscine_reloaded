354d0be907905b17e49b486bcc4646b63209bcf8
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-13 15:46:28 -0300

    Implementa ex08.

---
+++ ex08/ft_is_negative.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/13 15:44:16 by fconde-p          #+#    #+#             */
+/*   Updated: 2025/07/13 15:45:46 by fconde-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+	{
+		ft_putchar('N');
+	}
+	else
+	{
+		ft_putchar('P');
+	}
+}
+
+int	main(void)
+{
+	ft_is_negative(-1);
+	ft_is_negative(0);
+	ft_is_negative(1);
+	ft_is_negative(10);
+}
