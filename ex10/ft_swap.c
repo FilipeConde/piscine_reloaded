db90cd4c4102a83036f03896a693a2042bf4b5d8
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-13 17:23:15 -0300

    Ajusta arquivo para entrega de acordo com a norma.

--- ex10/ft_swap.c
+++ ex10/ft_swap.c
@@ -6,11 +6,9 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 17:01:57 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/13 17:20:23 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/13 17:22:42 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 void	ft_swap(int *a, int *b)
 {
@@ -19,20 +17,4 @@ void	ft_swap(int *a, int *b)
 	temp = *a;
 	*a = *b;
 	*b = temp;
-}
-
-int	main(void)
-{
-	int	a;
-	int	b;
-	int	*ptr_a;
-	int	*ptr_b;
-
-	a = 4;
-	b = 2;
-	ptr_a = &a;
-	ptr_b = &b;
-	printf("%d%d\n", a, b);
-	ft_swap(ptr_a, ptr_b);
-	printf("%d%d\n", a, b);
 }
