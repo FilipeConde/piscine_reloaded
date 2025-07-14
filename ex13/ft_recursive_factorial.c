efb23a2af1eec5d0a6b4742cfe87387db3b2b1c5
Filipe Conde <filipecondep@gmail.com>
2025-07-13 22:56:24 -0300

    Implementa fatorial recursiva para ex13.

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb == 0 || nb == 1)
+	{
+		return 1;
+	}
+	if (nb < 0)
+	{
+		return  0;
+	}
+	return nb * ft_recursive_factorial(nb - 1);
+}
+
+int	main(void)
+{
+	int	to_print;
+
+	to_print = ft_recursive_factorial(1);
+	printf("%d\n", to_print);
+}
