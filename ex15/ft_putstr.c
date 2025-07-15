edfd326dda5a54f9191de421641ef143e424ebc4
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-15 10:37:15 -0300

    Implementa ft_putstr.

---
+++ ex15/ft_putstr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 10:24:04 by fconde-p          #+#    #+#             */
+/*   Updated: 2025/07/15 10:35:07 by fconde-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		*str++;
+	}
+}
+
+int	main(void)
+{
+	char	string[25] = "teste";
+	ft_putstr(string);
+}
