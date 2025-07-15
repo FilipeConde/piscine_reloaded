b28fa25c457266474bb3b465e2e828f5e78c2551
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-15 11:29:35 -0300

    Ajusta arquivo para entrega de acordo com a norma.

--- ex17/ft_strcmp.c
+++ ex17/ft_strcmp.c
@@ -6,12 +6,9 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 11:07:16 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/15 11:28:05 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/15 11:29:04 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-#include <string.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -28,12 +25,4 @@ int	ft_strcmp(char *s1, char *s2)
 		return (s2[i] * (-1));
 	else
 		return (s1[i] * 1);
-}
-
-int	main(void)
-{
-	char	str1[25] = "teste ! ";
-	char    str2[25] = "teste !";
-	printf("%d\n", ft_strcmp(str1, str2));
-	printf("%d\n", strcmp(str1, str2));
 }
