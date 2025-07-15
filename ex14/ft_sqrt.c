214e58fc0243e5600e87e69ee417627d7f909cd2
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-15 10:22:34 -0300

    Ajusta arquivo para entrega de acordo com a norma.

--- ex14/ft_sqrt.c
+++ ex14/ft_sqrt.c
@@ -6,11 +6,9 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 08:32:03 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/15 10:18:36 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/15 10:22:00 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 int	ft_sqrt(int nb)
 {
@@ -21,23 +19,9 @@ int	ft_sqrt(int nb)
 	{
 		if (curr_num * curr_num == nb)
 		{
-			return curr_num;
+			return (curr_num);
 		}
 		curr_num--;
 	}
-	return 0;
-}
-
-int	main(void)
-{
-	int	result;
-
-	result = ft_sqrt(8);
-	printf("%d\n", result);
-
-	result = ft_sqrt(-9);
-        printf("%d\n", result);
-
-	result = ft_sqrt(225);
-        printf("%d\n", result);
+	return (0);
 }
