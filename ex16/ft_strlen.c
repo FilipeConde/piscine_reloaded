2ac9940dafc16144d8cb29b3cac19424ff674af9
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-15 11:01:55 -0300

    Prepara arquivo para entrega.

--- ex16/ft_strlen.c
+++ ex16/ft_strlen.c
@@ -6,12 +6,9 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 10:45:58 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/15 10:57:41 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/15 10:58:57 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-#include <string.h>
 
 int	ft_strlen(char *str)
 {
@@ -24,12 +21,4 @@ int	ft_strlen(char *str)
 		str++;
 	}
 	return (length);
-}
-
-int	main(void)
-{
-	char	string[25] = " tes 564 te";
-
-	printf("%d\n", ft_strlen(string));
-	printf("%ld\n", strlen(string));
 }
