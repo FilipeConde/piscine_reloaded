2410e62e9daf28258943101bb02f30b69e258e0f
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-15 10:58:32 -0300

    Valida testando com a função original.

--- ex16/ft_strlen.c
+++ ex16/ft_strlen.c
@@ -6,11 +6,12 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 10:45:58 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/15 10:51:19 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/15 10:57:41 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 
 int	ft_strlen(char *str)
 {
@@ -27,7 +28,8 @@ int	ft_strlen(char *str)
 
 int	main(void)
 {
-	char	string[25] = "teste";
+	char	string[25] = " tes 564 te";
 
 	printf("%d\n", ft_strlen(string));
+	printf("%ld\n", strlen(string));
 }
