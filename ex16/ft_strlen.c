289bb2b6fd9d2cea55074e3d3545555e98cd7f72
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-15 10:53:20 -0300

    Implementa ft_strlen.

---
+++ ex16/ft_strlen.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 10:45:58 by fconde-p          #+#    #+#             */
+/*   Updated: 2025/07/15 10:51:19 by fconde-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	length;
+
+	length = 0;
+	while (*str)
+	{
+		length++;
+		str++;
+	}
+	return (length);
+}
+
+int	main(void)
+{
+	char	string[25] = "teste";
+
+	printf("%d\n", ft_strlen(string));
+}
