9f926d7c5a0353dff06021b156d5f7b9b0001d37
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-15 21:37:06 -0300

    Inclui cabeçalho.

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 20:14:06 by fconde-p          #+#    #+#             */
+/*   Updated: 2025/07/15 20:16:55 by fconde-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_iterative_factorial(int nb)
 {
 	int	result;
 
 	if (nb == 0)
 	{
-		return 1;
+		return (1);
 	}
 	if (nb < 0)
 	{
-		return  0;
+		return (0);
 	}
 	result = nb;
 	while (nb > 1)
@@ -16,5 +28,5 @@ int	ft_iterative_factorial(int nb)
 		result *= (nb - 1);
 		nb--;
 	}
-	return result;
+	return (result);
 }
--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 20:14:44 by fconde-p          #+#    #+#             */
+/*   Updated: 2025/07/15 20:18:19 by fconde-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_recursive_factorial(int nb)
 {
 	if (nb == 0 || nb == 1)
 	{
-		return 1;
+		return (1);
 	}
 	if (nb < 0)
 	{
-		return  0;
+		return (0);
 	}
-	return nb * ft_recursive_factorial(nb - 1);
+	return (nb * ft_recursive_factorial(nb - 1));
 }
