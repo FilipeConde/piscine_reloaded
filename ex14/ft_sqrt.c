fdb33d2d576d88651988d7b5fd81277bbc7ef6ad
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-15 22:00:16 -0300

    Revisa implementação da função.

--- ex14/ft_sqrt.c
+++ ex14/ft_sqrt.c
@@ -6,10 +6,13 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 08:32:03 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/15 10:22:00 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/15 21:57:35 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*#include <stdio.h>
+#include <math.h>
+*/
 int	ft_sqrt(int nb)
 {
 	int	curr_num;
@@ -25,3 +28,19 @@ int	ft_sqrt(int nb)
 	}
 	return (0);
 }
+/*
+int	main(void)
+{
+	int	n;
+	int	result;
+	
+	n = 1;
+	result = ft_sqrt(n);
+	printf("%d\n", result);
+
+	n = 8;
+	printf("%d\n", ft_sqrt(n));
+
+	n = 121;
+	printf("%d\n", ft_sqrt(n));
+}*/
