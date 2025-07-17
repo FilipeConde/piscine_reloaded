1711682612e4446f956049dc6d08f818470e53b3
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-17 08:58:00 -0300

    Cria estrutura para ft_sort_params (strcmp e a leitura e impressão dos parâmetros).

---
+++ ex19/ft_sort_params.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 08:40:33 by fconde-p          #+#    #+#             */
+/*   Updated: 2025/07/17 08:57:04 by fconde-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(c char)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		while (*argv[i])
+		{
+			ft_putchar(*argv[i]);
+			argv[i]++;
+		}
+		ft_putchar('\n');
+		i++;
+	}	
+}
