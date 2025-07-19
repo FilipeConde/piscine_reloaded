0e0d0bd4ba052741776e1b3f6016520a0d2471b1
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-19 13:09:47 -0300

    Altera algoritmo para partir do menor número possível.

--- ex14/ft_sqrt.c
+++ ex14/ft_sqrt.c
@@ -3,44 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sqrt.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fconde-p <fconde-p@student.42sp.org.b      +#+  +:+       +#+        */
+/*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 08:32:03 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/15 21:57:35 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/19 13:08:47 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include <stdio.h>
-#include <math.h>
-*/
+// #include <math.h>
+
 int	ft_sqrt(int nb)
 {
-	int	curr_num;
+	int	sqroot;
 
-	curr_num = nb / 2;
-	while (curr_num > 1)
+	if (nb < 1)
 	{
-		if (curr_num * curr_num == nb)
+		return (0);
+	}
+	sqroot = 1;
+	while (sqroot >= 1)
+	{
+		if (sqroot * sqroot == nb)
 		{
-			return (curr_num);
+			return (sqroot);
 		}
-		curr_num--;
+		sqroot++;
 	}
 	return (0);
 }
-/*
-int	main(void)
-{
-	int	n;
-	int	result;
-	
-	n = 1;
-	result = ft_sqrt(n);
-	printf("%d\n", result);
 
-	n = 8;
-	printf("%d\n", ft_sqrt(n));
-
-	n = 121;
-	printf("%d\n", ft_sqrt(n));
-}*/
+// int	main(void)
+// {
+// 	int	n;
+// 	int	result;
+// 	n = 1;
+// 	result = ft_sqrt(n);
+// 	printf("%d\n", result);
+// 	n = 0;
+// 	printf("%d\n", ft_sqrt(n));
+// 	n = -4;
+// 	printf("%d\n", ft_sqrt(n));
+// 	n = 8;
+// 	printf("%d\n", ft_sqrt(n));
+// 	n = 330123790096;
+// 	printf("%d\n", ft_sqrt(n));
+// }
