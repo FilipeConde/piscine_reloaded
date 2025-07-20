dfdae7bda1d38d6b9e4a28042b6c6217aa886f06
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-20 19:41:27 -0300

    Comenta main.

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 11:13:27 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/20 19:34:33 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/20 19:39:50 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ char	*ft_strdup(char *src)
 	result[i] = '\0';
 	return (result);
 }
-
+/*
 #include <stdio.h>
 #include <string.h>
 int	main(void)
@@ -59,4 +59,4 @@ int	main(void)
 	new_str = strdup(src);
 	printf("%s\n", src);
 	printf("%s\n", new_str);
-}
+}*/
