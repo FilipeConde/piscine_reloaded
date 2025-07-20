99dcde91e33087a34a66f611afeca001c4b842e5
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-20 11:31:20 -0300

    Ajusta ft_range() para retornar NULL pointer em caso de entradas iguais ou invertidas.

--- ex21/ft_range.c
+++ ex21/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 14:10:53 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/19 21:08:10 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/20 11:26:48 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,10 @@ int	*ft_range(int min, int max)
 	j = 0;
 	qty = max - min;
 	int_arr = (int *) malloc (qty * sizeof(int));
+	if (min >= max)
+	{
+		return ((void *)0);
+	}
 	i = min;
 	while (i < max)
 	{
@@ -31,20 +35,28 @@ int	*ft_range(int min, int max)
 	}
 	return (int_arr);
 }
-/*
-#include <stdio.h>
-int	main(void)
-{
-	int	*arr_ptr;
-	int	i;
 
-	i = 10;
-	arr_ptr = ft_range(-5, 5); 
-
-	while (*arr_ptr < i)
-	{
-		printf(" %d |", *arr_ptr);
-		arr_ptr++;
-	}
-}
-*/
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	int	*arr_ptr;
+// 	int	i;
+	//
+// 	i = 10;
+// 	arr_ptr = ft_range(-5, 5); 
+	//
+// 	if (arr_ptr == NULL)
+// 	{
+//         printf("NULL Pointer Passed");
+//         // return;
+//     }
+// 	else
+// 	{
+// 		printf("Non-Null Pointer Passed");
+// 	}
+// 	while (*arr_ptr < i)
+// 	{
+// 		printf(" %d |", *arr_ptr);
+// 		arr_ptr++;
+// 	}
+// 	}
