62b9abaa718ece96c814d6f7ed47f17b54eb3ea3
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-20 11:14:03 -0300

    Ajusta formatação de acordo com a norma.

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 11:13:27 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/20 11:07:54 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/20 11:11:20 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,10 @@ int	ft_strlen(char *str)
 char	*ft_strdup(char *src)
 {
 	char	*result;
-	int	i;
+	int		i;
 
 	i = 0;
 	result = (char *) malloc(ft_strlen(src) * sizeof(char));
-//	New aproach: copy the content instead to set the mem address.
-//	result = src;
 	if (src[0] == '\0')
 	{
 		result[0] = '\0';
@@ -44,21 +42,20 @@ char	*ft_strdup(char *src)
 		result[i] = src[i];
 		i++;
 	}
-	
 	return (result);
 }
 
-#include <stdio.h>
-#include <string.h>
-int	main(void)
-{
-	char	*src = "\0";
-	char	*new_str;
-	new_str = ft_strdup(src);
-	printf("%s\n", src);
-	printf("%s\n\n", new_str);
+// #include <stdio.h>
+// #include <string.h>
+// int	main(void)
+// {
+// 	char	*src = "\0";
+// 	char	*new_str;
+// 	new_str = ft_strdup(src);
+// 	printf("%s\n", src);
+// 	printf("%s\n\n", new_str);
 
-	new_str = strdup(src);
-	printf("%s\n", src);
-	printf("%s\n", new_str);
-}
+// 	new_str = strdup(src);
+// 	printf("%s\n", src);
+// 	printf("%s\n", new_str);
+// }
